02085230844fe030ab1eb0a7f372bd97425185ed
Maria Lucia Gomez Rodiguez <80794477+Juancamilo3821@users.noreply.github.com>
2025-08-11 12:28:40 -0500

    (fixed) fixed json firebase

--- config/firebaseAdmin.js
+++ config/firebaseAdmin.js
@@ -1,5 +1,5 @@
 const admin = require('firebase-admin');
-const serviceAccount = require('./drteeth-service-account.json'); // key
+const serviceAccount = require('./drteethapp-service.json'); // key
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
